31e1bb2267dcf8dc5a92847dc17d3b0052b8d690
karim <karimtarekabdelaziz@gmail.com>
2024-09-28 18:37:10 +0300

    Feat: Toggle between light and dark

---
+++ src/components/RenderSVG.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { iconName, sizes } = defineProps<{
+  iconName: string;
+  sizes?: string;
+  fill?: string;
+}>();
+</script>
+
+<template>
+  <svg :class="[`w-[1.5625rem] h-[1.5625rem] ${sizes} `]">
+    <use :xlink:href="`/icons/sprite.svg#${iconName}`" :style="`fill:${fill}`">
+      <title>{{ iconName }}</title>
+    </use>
+  </svg>
+</template>
--- src/components/ToggleMode.vue
+++ src/components/ToggleMode.vue
@@ -1,15 +1,20 @@
 <template>
   <button
     @click="toggleHandler"
-    class="relative w-20 h-8 border rounded-full border-primary-2/80 border-1"
+    class="relative flex items-center justify-between w-20 h-8 px-1 border rounded-full border-primary-2/80 border-1"
   >
-    <span :class="mergeClasses(classes, moveClass)"></span>
+    <RenderSVG iconName="dark-mode" sizes="h-[5rem]" fill="white" />
+    <RenderSVG iconName="light-mode" sizes="h-[5rem]" fill="white" />
+    <span :class="mergeClasses(classes, moveClass)">
+      <RenderSVG iconName="dark-mode" sizes="h-[5rem]" />
+      <!-- <RenderSVG iconName="light-mode" sizes="h-[5rem]" /> -->
+    </span>
   </button>
 </template>
 
 <script lang="ts" setup>
 const classes =
-  "absolute transition-transform block w-10 h-10 rounded-full bg-primary-2 -start-1 top-1/2 -translate-y-2/4";
+  "absolute flex items-center justify-center transition-transform block w-10 h-10 rounded-full bg-primary-2 -start-1 top-1/2 -translate-y-2/4";
 const moveClass = ref("translate-x-0");
 
 const toggleHandler = () => {
