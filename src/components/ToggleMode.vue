73282d444343cd0ca4fd78430951d1c52226a1f3
karim <karimtarekabdelaziz@gmail.com>
2024-09-28 21:59:45 +0300

    Feat: Toggle between light and dark

--- src/components/ToggleMode.vue
+++ src/components/ToggleMode.vue
@@ -14,36 +14,7 @@
 </template>
 
 <script lang="tsx" setup>
-const moveClass = ref("translate-x-0");
-const cookie = useCookie("theme");
-
-// ------------ Computed ------------ //
-const isDark: ComputedRef<boolean> = computed(() => cookie.value === "dark");
-
-// ------------ Functions ------------ //
-const setModeHandler = (): void => {
-  cookie.value = cookie.value === "dark" ? "light" : "dark";
-};
-
-const moveHandler = (): void => {
-  if (typeof cookie.value === "undefined") cookie.value = "dark";
-  moveClass.value = isDark.value ? "translate-x-0" : "translate-x-12";
-};
-
-const toggleHandler = (): void => {
-  setModeHandler();
-  moveHandler();
-};
-
-moveHandler();
-
-useHead(() => {
-  return {
-    bodyAttrs: {
-      "data-theme": cookie.value || "dark",
-    },
-  };
-});
+const { isDark, moveClass, toggleHandler } = useToggleTheme();
 
 // ------------ Classes ------------ //
 const buttonClasses =
