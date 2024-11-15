9c5ae03bacf67db5a201de39d287193bbaa9d131
karim <karimtarekabdelaziz@gmail.com>
2024-11-15 16:17:45 +0200

    Update

--- src/components/sections/Experience.vue
+++ src/components/sections/Experience.vue
@@ -7,6 +7,10 @@
         <div class="timeline__wrapper__timeline"></div>
         <div class="timeline__wrapper__line" />
       </div>
+      <div class="timeline__wrapper">
+        <div class="timeline__wrapper__timeline"></div>
+        <div class="timeline__wrapper__line" />
+      </div>
     </div>
   </section>
 </template>
@@ -18,9 +22,9 @@
   @apply flex flex-col items-center;
 }
 .timeline__wrapper__timeline {
-  @apply flex items-center justify-center bg-blue-500 border-2 border-blue-500 rounded-full after:rounded-full after:w-3 after:h-3 after:bg-black w-7 h-7;
+  @apply flex items-center justify-center bg-primary-2 rounded-full after:rounded-full after:w-3 after:h-3 after:bg-primary-1 w-7 h-7;
 }
 .timeline__wrapper__line {
-  @apply w-[2px] h-8 my-3 bg-blue-500;
+  @apply w-[2px] h-16 my-3 bg-primary-2;
 }
 </style>
