6ed9359c9d566c5f71974dc3b5ac676fcb1547d6
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-05-28 02:56:08 +0300

    Refactor configuration files for consistency and update experience section with detailed job entries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  srcDir: "src/",
+  compatibilityDate: '2024-04-03',
+  srcDir: 'src/',
   devtools: { enabled: true },
   app: {
     head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-      title: "karim tarek",
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+      title: 'karim tarek',
     },
   },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
-  css: ["~/assets/style/main.css"],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  css: ['~/assets/style/main.css'],
 
   googleFonts: {
-    display: "block",
+    display: 'block',
     families: {
       Poppins: [400, 500, 600],
     },
     preload: true,
   },
-});
+})
