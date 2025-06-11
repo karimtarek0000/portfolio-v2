abb5e14ba7ab7bd5a11aed5396f364af722402a9
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-06-11 19:53:30 +0300

    feat: Update favicon links and manifest for enhanced branding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,28 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
       title: 'karim tarek',
+
+      link: [
+        { rel: 'icon', href: '/favicons/favicon.ico', sizes: '48x48' },
+        {
+          rel: 'icon',
+          href: '/favicons/favicon.svg',
+          sizes: 'any',
+          type: 'image/svg+xml',
+        },
+        {
+          rel: 'icon',
+          href: '/favicons/favicon.svg',
+          sizes: '96x96',
+          type: 'image/svg+xml',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/favicons/apple-touch-icon.png',
+        },
+        { rel: 'manifest', href: '/favicons/site.webmanifest' },
+      ],
     },
   },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-swiper'],
