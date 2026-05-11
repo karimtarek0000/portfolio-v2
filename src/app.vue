e534452450452fe7d5aa83eb01dc1af14db6ed4f
Karim Tarek abdel aziz <43219770+karimtarek0000@users.noreply.github.com>
2026-05-11 16:20:36 +0300

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -3,7 +3,7 @@ const data = useState('data')
 
 await callOnce(async () => {
   const { data: fetchedData } = await useFetch(
-    'https://jsonblob.com/api/jsonBlob/019d77b3-00f2-7555-85df-569b41b21e75',
+    'https://jsonblob.com/api/jsonBlob/019e1731-aa38-70a8-9876-93bc40ad0364',
     {
       key: 'data-portfolio',
     },
